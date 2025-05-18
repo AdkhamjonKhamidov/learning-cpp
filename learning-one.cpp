7455fe39722669046b0697a83dc683f29d5b7246
Adkhamjon Khamidov <adkhamjonkhamidov@gmail.com>
2025-05-18 18:41:11 -0400

    compiler version

--- learning-one.cpp
+++ learning-one.cpp
@@ -3,5 +3,10 @@
 using namespace std;
 
 int main (){
-	cout << "hello" << endl;
+	if (__cplusplus == 202002L) cout << "c++20" << endl;
+	else if (__cplusplus == 201703L) cout << "c++17" << endl;
+	else if (__cplusplus == 201402L) cout << "c++14" << endl;
+	else if (__cplusplus == 201103L) cout << "c++11"<< endl;
+	else if (__cplusplus == 199711L) cout << "c++97" << endl;
+	else cout << "damn" << endl; 
 }
