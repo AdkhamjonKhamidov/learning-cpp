714c929ffb2d2d8ae059617630a09076473774a5
Adkhamjon Khamidov <adkhamjonkhamidov@gmail.com>
2025-05-18 22:20:34 -0400

    reviewing initialization

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream> 
 
 int main (){
-	std::string name; //user's name
-	std::cout << "Hello" << "\nMy name is: ";
+	std::string name{"Adam"}; //user's name list initialization -- prefered way to initialize in modern cpp 
+	std::cout << "Hello " << name << "\nMy name is: ";
 	std::cin >> name; //input user's name 
 	std::cout << "Hiii " << name << " how are you?" <<std::endl;
 }
