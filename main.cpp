3b14dc9517248c33716283808d7bfd4cd167cbcd
Adkhamjon Khamidov <adkhamjonkhamidov@gmail.com>
2025-05-18 23:51:20 -0400

    more basic review: cout cin \n endl

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 
 int main (){
 	std::string name{"Adam"}; //user's name list initialization -- prefered way to initialize in modern cpp 
-	std::cout << "Hello " << name << "\nMy name is: ";
-	std::cin >> name; //input user's name 
-	std::cout << "Hiii " << name << " how are you?" <<std::endl;
+	std::string lastName{" Khamidov"}; 
+	std::cout << "Hello " << name << lastName << "\nMy name is: "; // \n is faster than endl as it doesn't flush the buffer
+	std::cin >> name >> lastName; //input user's name 
+	std::cout << "Hiii " << name << " " << lastName << " how are you?" << '\n'; //single char use single quotes ''
 }
