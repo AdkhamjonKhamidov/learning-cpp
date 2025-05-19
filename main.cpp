2bc675a0bd7a9de09c2cab50e7efb94eb9e2f1f7
Adkhamjon Khamidov <adkhamjonkhamidov@gmail.com>
2025-05-18 21:37:38 -0400

    reviewing basics: cout/cin

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream> 
+
+int main (){
+	std::string name; //user's name
+	std::cout << "Hello" << "\nMy name is: ";
+	std::cin >> name; //input user's name 
+	std::cout << "Hiii " << name << " how are you?" <<std::endl;
+}
