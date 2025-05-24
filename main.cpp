08cde6df2a969f9cfa2a98f1cc25c24fa31ea785
Adkhamjon Khamidov <adkhamjonkhamidov@gmail.com>
2025-05-24 18:37:12 -0400

    learning about signed and unsigned fixed with int

--- main.cpp
+++ main.cpp
@@ -1,26 +1,11 @@
-#include <plog/Log.h> 
-#include <plog/Initializers/RollingFileInitializer.h> 
 #include <iostream>
+#include <cstdint> 
 
-int getUserInput()
+int main()
 {
-	PLOGD << "getUserInput() called"; //plogd is defined by plog lib;
-					  //clog is std c++ for loging but only does console output where as plog is for a seperate log files
-					  //as a side, clog uses cerr to write to standard error stream	
-	std::cout << "Enter a number: ";
-	int x{};
-	std::cin >> x;
-	return x;
-}
-
-int main ()
-{
-	plog::init(plog::debug, "Logfile.txt"); // initilize the plog logger
-	
-	PLOG << "main() called";
-
-	int x{getUserInput()};
-	std::cout << "You entered: " << x << '\n';
-
+	std::int32_t signedBit32{2147483647};
+	std::uint32_t unsignedBit32{4294967295};
+	std::cout << "largest signed 32 bit int: " << signedBit32 
+		<< "\nlargest un-signed 32 bit int: " << unsignedBit32 << '\n';
 	return 0;
 }
