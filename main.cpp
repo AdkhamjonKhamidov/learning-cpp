3d99076b43b163fcec5c9dcbb70e518e32a1fc46
Adkhamjon Khamidov <adkhamjonkhamidov@gmail.com>
2025-06-03 23:59:28 -0400

    checking if i still remember basics

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
-#include <cstdint> 
+#include <string_view>
+#include <string>
+
+void print(std::string_view output)
+{
+	std::cout << output << '\n';
+}
 
 int main()
 {
-	std::int32_t signedBit32{2147483647};
-	std::uint32_t unsignedBit32{4294967295};
-	std::cout << "largest signed 32 bit int: " << signedBit32 
-		<< "\nlargest un-signed 32 bit int: " << unsignedBit32 << '\n';
+	std::string output{"Hello world!"};
+	print(output);
 	return 0;
 }
